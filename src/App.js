cc6f9d32254eb77f5facf8157c3e9742e603439b
Altyn Berdiyeva <aberdiyeva@LISAACADEMY.ORG>
2020-03-13 10:53:17 -0500

    fixed some code here

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+// import ReactDOM from "react-dom";
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route, BrowserRouter as Router} from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+    
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      </Router>
   </div>
 );
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import {Link, BrowserRouter as Router} from 'react-router-dom';
+// import ReactDOM from "react-dom";
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+        <Router>
+          <div className="nav">
+            <div className="Nav-link">
+              <Link to="/">Home</Link>
+            </div>
+            <div className="Nav-link">
+              <Link to="/about">About</Link>
+            </div>
+            <div className="Nav-link">
+              <Link to="/contact">Contact</Link>
+            </div>
+          </div>
+        </Router>
       </div>
     </div>
   );
